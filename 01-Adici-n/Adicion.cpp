fa1b7ce16bdc9f26109071f047be783535fcad64
Cristian Siles <37933034+cristian308@users.noreply.github.com>
2018-04-15 21:51:29 -0300

    Add files via upload

---
+++ 01-Adici-n/Adicion.cpp
@@ -0,0 +1,18 @@
+/* Cristian Siles
+ * 20180415
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int a;
+cout<<"Ingrese Primer valor"<<endl;
+cin >> a;
+int b;
+cout<<"Ingrese Segundo valor"<<endl;
+cin >> b;
+cout << a + b;
+}
